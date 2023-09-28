5668d0ec0774fb1f61c5aea47b2a004668298f97
Dianaalejandra1446 <dianaleja04@hotmail.com>
2023-09-27 21:31:13 -0500

    avance hoy

---
+++ index.js
@@ -0,0 +1,20 @@
+const inputCantidad = document.getElementById('cantidad');
+const btnGenerar = document.getElementById('generar');
+const listaNum = document.getElementById('lista');
+
+function generarFibonacci() {
+    listaNum.innerHTML= "";
+    let n=parseInt(inputCantidad.value);//Convertir a entero
+    let a=0; b=1;
+    while(n > 0){
+        const nuevoElement = document.createElement('li');
+        nuevoElement.textContent = a;
+        listaNum.appendChild(nuevoElement)
+        let  temp = a;
+        a = b;
+        b = temp + b;
+        n--
+    }
+
+}
+btnGenerar.addEventListener('click',generarFibonacci);
