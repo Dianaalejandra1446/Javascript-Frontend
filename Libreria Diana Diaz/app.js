1bdefed1f9076fe4961f8f57031d0422a206118e
Dianaalejandra1446 <dianaleja04@hotmail.com>
2023-10-06 21:24:01 -0500

    practica clase

---
+++ Libreria Diana Diaz/app.js
@@ -0,0 +1,13 @@
+const d = document;
+
+function agregarLibro(){
+    //Llamamos a los valores de los elementos directamente
+    let titulo = d.getElementById('titulo').value;
+    let autor =  d.getElementById('autor').value;
+    let publicacion =  d.getElementById('publicacion').value;
+    let disponible =  d.getElementById('disponible').value;
+    let prestado =  d.getElementById('prestado').value;
+    let portada = d.getElementById('portada_libro').value;
+
+    if()
+}
